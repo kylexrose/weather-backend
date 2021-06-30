a67948dfbb3c071fcf5314b248d53ad4825228f5
kylexrose@gmail.com <kylexrose@gmail.com>
2021-06-30 00:24:42 -0400

    initial commit

---
+++ routes/weather/WeatherModel.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const weatherSchema = new mongoose.Schema({
+  city: {
+    type: String,
+  },
+  country: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model("weather", weatherSchema);
